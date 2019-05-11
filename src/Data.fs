ad53e42c216b22b0fc747d0fc48f3d4dc4cb9cb3
Unknown <bdimpe@gmail>
2019-05-11 15:25:45 -0400

    weapon type is still duplicated

--- src/Data.fs
+++ src/Data.fs
@@ -23,14 +23,18 @@ let allTags =
     let modTags =
       sk.mods
       |> List.collect (fun m -> m.tags)
+      |> List.distinct
     sk.tags@modTags
+    |> List.distinct
   )
   |> List.distinct
   |> List.sortBy(fun x ->
     let isDamage = match x with |Damage _ -> true | _ -> false
     let isConversion = match x with |DirectConversion _ -> true | DoTConversion _ -> true | _ -> false
-    isConversion,isDamage, string x
+    isConversion, isDamage, string x
   )
 let allTagDisplays =
   allTags
   |> List.map (Tag.ToDisplay)
+
+printfn "%A" allTagDisplays
