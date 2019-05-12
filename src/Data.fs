f215b6c551dd1d2c414bc4469bf979dcbd36628a
Unknown <bdimpe@gmail>
2019-05-12 18:49:12 -0400

    some reactive utility!

--- src/Data.fs
+++ src/Data.fs
@@ -37,30 +37,24 @@ let gdClasses : GdClass list = [
   Classes.Other.NotImplemented.Champion.cls
   Classes.Other.NotImplemented.Riftstalker.cls
   Classes.Other.NotImplemented.Necrotic.cls
-
 ]
+
+let getClassTags (cls:GdClass) =
+    cls.skills
+    |> List.collect (fun sk ->
+      let modTags =
+        sk.mods
+        |> List.collect (fun m -> m.tags)
+      sk.tags@modTags
+    )
 
 let allTags =
   gdClasses
-  |> List.collect(fun gc ->
-    gc.skills
-  )
-  |> List.collect (fun sk ->
-    let modTags =
-      sk.mods
-      |> List.collect (fun m -> m.tags)
-      |> List.distinct
-    sk.tags@modTags
-    |> List.distinct
-  )
-  |> List.distinct
+  |> List.collect getClassTags
+  |> List.distinctBy(Tag.ToDisplay)
   |> List.sortBy(fun x ->
     let isDamage = match x with |Damage _ -> true | _ -> false
     let isConversion = match x with |DirectConversion _ -> true | DoTConversion _ -> true | _ -> false
     isConversion, isDamage, string x
   )
-let allTagDisplays =
-  allTags
-  |> List.map (Tag.ToDisplay)
-
-printfn "%A" allTagDisplays
+let getTagDisplays = List.map (Tag.ToDisplay)
--- src/GdtApp.fs
+++ src/GdtApp.fs
@@ -19,39 +19,23 @@ open Components
 // type Message =
 //     |ChangeClassFilter of string
 
-type AppProps = AppProps
-type AppState = {ClassFilter : string}
-
 let classDrop onChange =
     let kvs = gdClasses |> List.map(fun cls -> cls.name, cls.name)
     select (("No class filter",""):: kvs) None onChange
-type App(initProps:AppProps) =
-    inherit React.Component<AppProps,AppState>(initProps)
-    do base.setInitState {ClassFilter=""}
-    override x.render() =
-        div [
 
-        ] [
-            str x.state.ClassFilter
-            classDrop (fun e ->
-                printfn "Changing"
-                x.setState(fun s p ->
-                    {s with ClassFilter=e}))
-        ]
-let inline app props = ofType<App,_,_> props []
-let content:React.ReactElement =
+let gdTable allTags classes : React.ReactElement =
+    let allTagDisplays = getTagDisplays allTags
     div [Class "container-fluid"] [
-        app(AppProps)
         table [Class "table table-dark"] [
             thead [] [
                 tr [] [
-                    yield th [Scope "col"] [str "Class"]
+                    yield th [Scope "col"] [str <| sprintf "Class(%i)" gdClasses.Length]
                     yield! allTagDisplays
                         |> List.map(fun name -> th [Scope "col"][str name])
                 ]
             ]
             tbody [] (
-                gdClasses
+                classes
                 |> List.map(fun cls ->
                     tr [Class <| string cls.theme][
                         yield td[][
@@ -69,6 +53,37 @@ let content:React.ReactElement =
                     ]
                 )
             )
-
         ]
     ]
+type AppProps = AppProps
+type AppState = {ClassFocus: string;ClassCount:int}
+
+type App(initProps:AppProps) =
+    inherit React.Component<AppProps,AppState>(initProps)
+    do base.setInitState {ClassFocus="";ClassCount=gdClasses.Length}
+    member x.getFocusedClass() =
+        if System.String.IsNullOrWhiteSpace x.state.ClassFocus then
+            None
+        else
+            gdClasses |> List.tryFind(fun c -> c.name = x.state.ClassFocus)
+    member x.getIncludedTags() =
+        // include only tags that the focused class has
+        match x.getFocusedClass() with
+        | None -> allTags
+        | Some cls -> getClassTags cls |> List.distinctBy Tag.ToDisplay
+
+    override x.render() =
+        div [
+
+        ] [
+            str x.state.ClassFocus
+            classDrop (fun e ->
+                printfn "Changing"
+                x.setState(fun s p ->
+                    {s with ClassFocus=e}))
+            gdTable (x.getIncludedTags()) gdClasses
+        ]
+
+let inline app props = ofType<App,_,_> props []
+
+let content:React.ReactElement = app AppProps
